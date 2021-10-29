9eb9947c277c5b63bcc9468aae74c355959a90c3
HaoHuynh0301 <hao152903@gmail.com>
2021-10-29 19:32:07 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
- return 'Hello FOSS'
+ return 'Hello FOSS, Hello CI/CD using Jenkins.'
 
 if __name__ == '__main__':
  app.run(debug=True,host='0.0.0.0')
